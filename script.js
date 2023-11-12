39948d5d17339b1325b60f4c4f304857a85d75e7
Enes <128540843+enessbyram@users.noreply.github.com>
2023-11-12 20:30:28 +0300

    one bug fixed

--- script.js
+++ script.js
@@ -1,66 +1,57 @@
-let myNotes = []
-const inputEl = document.getElementById("inputEl")
-const addBtn = document.getElementById("addBtn")
-let list = document.getElementById("list")
-const clearBtn = document.getElementById("clearBtn")
+let myNotes = [];
+const inputEl = document.getElementById("inputEl");
+const addBtn = document.getElementById("addBtn");
+let list = document.getElementById("list");
+const clearBtn = document.getElementById("clearBtn");
 
 function renderList() {
-    let listItems = ""
+    let listItems = "";
     for (let i = 0; i < myNotes.length; i++) {
-        listItems += `<div class='small-container'><li class='item'>${myNotes[i]}</li><button class='doneBtn'>DONE</button></div>`
+        listItems += `<div class='small-container'><li class='item'>${myNotes[i]}</li><button class='doneBtn'>DONE</button></div>`;
     }
-    list.innerHTML = listItems
+    list.innerHTML = listItems;
+
+    let doneBtns = document.querySelectorAll(".doneBtn");
+    let items = document.querySelectorAll(".item");
+
+    for (let i = 0; i < doneBtns.length; i++) {
+        doneBtns[i].addEventListener('click', function () {
+            let currentBtn = doneBtns[i];
+            let currentItem = items[i];
+
+            if (currentBtn.textContent === "DONE") {
+                currentItem.classList.add("done");
+                currentBtn.textContent = "UNDONE";
+            } else {
+                currentItem.classList.remove("done");
+                currentBtn.textContent = "DONE";
+            }
+
+            updateCompletedNotes();
+        });
+    }
 }
 
 addBtn.addEventListener("click", () => {
     if (inputEl.value === "") {
-        inputEl.textContent = "You need to enter your note first!"
+        inputEl.textContent = "You need to enter your note first!";
     } else {
-        myNotes.push(inputEl.value)
+        myNotes.push(inputEl.value);
+        localStorage.setItem("myNotes", JSON.stringify(myNotes));
+        renderList();
     }
-    inputEl.value = ""
-    localStorage.setItem("myNotes", JSON.stringify(myNotes))
-    renderList()
-})
+    inputEl.value = "";
+});
 
 clearBtn.addEventListener("click", () => {
-    while (myNotes.length > 0) {
-        myNotes.pop()
-    }
-    list.innerHTML = " "
-    localStorage.clear()
-})
-
-let notesFromLocalStorage = JSON.parse(localStorage.getItem("myNotes"))
-
-if (notesFromLocalStorage) {
-    myNotes = notesFromLocalStorage
-    renderList()
-}
-
-let doneBtns = document.querySelectorAll(".doneBtn")
-let items = document.querySelectorAll(".item")
+    myNotes = [];
+    list.innerHTML = "";
+    localStorage.clear();
+});
 
 document.addEventListener("DOMContentLoaded", function () {
     loadCompletedNotes();
 });
-
-for (let i = 0; i < doneBtns.length; i++) {
-    doneBtns[i].addEventListener('click', function () {
-        let currentBtn = doneBtns[i];
-        let currentItem = items[i];
-
-        if (currentBtn.textContent === "DONE") {
-            currentItem.classList.add("done");
-            currentBtn.textContent = "UNDONE";
-        } else {
-            currentItem.classList.remove("done");
-            currentBtn.textContent = "DONE";
-        }
-
-        updateCompletedNotes();
-    });
-}
 
 function updateCompletedNotes() {
     let completedNotes = getCompletedNotes();
@@ -69,6 +60,8 @@ function updateCompletedNotes() {
 
 function getCompletedNotes() {
     let completedNotes = [];
+    let items = document.querySelectorAll(".item");
+
     for (let i = 0; i < items.length; i++) {
         if (items[i].classList.contains("done")) {
             completedNotes.push(i);
@@ -83,8 +76,18 @@ function loadCompletedNotes() {
     if (completedNotesFromStorage) {
         for (let i = 0; i < completedNotesFromStorage.length; i++) {
             let index = completedNotesFromStorage[i];
+            let items = document.querySelectorAll(".item");
+            let doneBtns = document.querySelectorAll(".doneBtn");
+
             items[index].classList.add("done");
             doneBtns[index].textContent = "UNDONE";
         }
     }
+}
+
+let notesFromLocalStorage = JSON.parse(localStorage.getItem("myNotes"));
+
+if (notesFromLocalStorage) {
+    myNotes = notesFromLocalStorage;
+    renderList();
 }
